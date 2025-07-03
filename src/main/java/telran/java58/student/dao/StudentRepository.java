4e20a6b6fbb24962b0e91e8aebad416683d290d5
edward <edwardlfish@gmail.com>
2025-07-03 21:02:03 +0300

    Update StudentRepository

--- src/main/java/telran/java58/student/dao/StudentRepository.java
+++ src/main/java/telran/java58/student/dao/StudentRepository.java
@@ -1,8 +1,17 @@
 package telran.java58.student.dao;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import telran.java58.student.model.Student;
 
-public interface StudentRepository extends MongoRepository<Student, Long> {
+import java.util.Set;
+import java.util.stream.Stream;
 
+public interface StudentRepository extends MongoRepository<Student, Long> {
+    Stream<Student> findByNameIgnoreCase(String name);
+
+    Long countByNameInIgnoreCase(Set<String> names);
+
+    @Query("{'scores.Math': {'$gt': 90}}")
+    Stream<Student> findByExamAndScoreGreaterThan(String exam, int score);
 }
--- src/main/java/telran/java58/student/service/StudentServiceImpl.java
+++ src/main/java/telran/java58/student/service/StudentServiceImpl.java
@@ -21,7 +21,7 @@ public class StudentServiceImpl implements StudentService {
 
     @Override
     public void addStudent(StudentCredentialsDto studentDto) {
-        if (studentRepository.findById(studentDto.getId()).isPresent()) {
+        if (studentRepository.existsById(studentDto.getId())) {
             throw new ConflictException();
         }
         Student student = new Student(studentDto.getId(), studentDto.getName(), studentDto.getPassword());
@@ -64,23 +64,19 @@ public class StudentServiceImpl implements StudentService {
 
     @Override
     public List<StudentDto> findStudentsByName(String name) {
-        return studentRepository.findAll().stream()
-                .filter(s -> name.equalsIgnoreCase(s.getName()))
+        return studentRepository.findByNameIgnoreCase(name)
                 .map(s -> new StudentDto(s.getId(), s.getName(), s.getScores()))
                 .toList();
     }
 
     @Override
     public Long countStudentByNames(Set<String> names) {
-        return studentRepository.findAll().stream()
-                .filter(s -> names.contains(s.getName()))
-                .count();
+        return studentRepository.countByNameInIgnoreCase(names);
     }
 
     @Override
     public List<StudentDto> findStudentsByExamNameMinScore(String examName, Integer minScore) {
-        return studentRepository.findAll().stream()
-                .filter(s -> s.getScores().containsKey(examName) && s.getScores().get(examName) > minScore)
+        return studentRepository.findByExamAndScoreGreaterThan(examName, minScore)
                 .map(s -> new StudentDto(s.getId(), s.getName(), s.getScores()))
                 .toList();
     }
